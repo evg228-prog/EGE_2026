5fc78559ea4d66b196eb9a5dbf0032a4e488bc8d
Genya <bornb228@gmail.com>
2025-11-24 17:18:09 +0200

    ...

---
+++ DZ_Task-14/12731.py
@@ -0,0 +1,9 @@
+from string import printable
+
+for x in printable[:13]:
+    num1 = int(f'9{x}AB', 13)
+    num2 = int(f'{x}46C', 16)
+    num3 = int(f'B7{x}', 15)
+    num = num1 + num2 - num3
+    if num % 14 == 0:
+        print(x, num // 14)
---
+++ DZ_Task-14/9918.py
@@ -0,0 +1,5 @@
+from string import printable as olo
+
+for x in range(1, 10):
+    for y in range(1, 10):
+
