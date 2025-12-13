b0209624a0836f0d53ef4310e18e4f04a4c4aebe
Genya <bornb228@gmail.com>
2025-12-13 15:48:11 +0200

    ...

--- DZ_Task-24/1273-re.py
+++ DZ_Task-24/1273-re.py
@@ -4,6 +4,6 @@ with open(r'files/24_1273 (1).txt') as file:
 
 pattern = r'(?<=XYZ).+?(?=XYZ)'
 matches = [match.group() for match in finditer(pattern, data)]
-print(len(max(matches, key=len)))
+print(len(max(matches, key=len)) + 4)
 
 # 305
