91c3273d342e059fa20c5863f51d4d31f9ddef88
Genya <bornb228@gmail.com>
2025-12-14 20:51:11 +0200

    ...

--- DZ_Task-24/1273-re.py
+++ DZ_Task-24/1273-re.py
@@ -3,14 +3,12 @@ from re import finditer
 with open(r'.\files\24_1273 (1).txt') as file:
     data = file.readline()
 
-pattern = r'(^|(?<=XYZ))).+?($|(?=(XYZ)))'
-
+pattern = r'(^|(?<=(XYZ))).+?($|(?=(XYZ)))'
 ans = 0
 for match in finditer(pattern, data):
     cnt_XYZ = match.groups().count('XYZ')
     ans = max(ans, len(match.group()) + 4 if cnt_XYZ == 2 else 2)
 
-Lengths = [len(match.group()) + 4 if match.groups().count('XYZ') == 2 else 2 for match in finditer(pattern, data)]
-print(max(Lengths))
+print(ans)
 
 # 305
